5e411657a9753062c5cd63546bfdb6a0f9137a77
Jonathanbez <jbbarroz@gmail.com>
2023-11-07 08:08:30 -0300

    Files to configure dev setup

--- config_setup_dev.sh
+++ config_setup_dev.sh
@@ -9,3 +9,23 @@ rvm fix-permissions
 su - $USER
 #Verificar se o mesmo está no ID
 id
+
+
+# Instalar e configurar ZSH
+sudo dnf install zsh -y
+chsh -s /bin/zsh
+zsh
+
+# Instalar Oh-my-zsh! -> https://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar Zsh Autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Instalar Zsh Syntax Highlighting
+# https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Mudar plugins
+# plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
