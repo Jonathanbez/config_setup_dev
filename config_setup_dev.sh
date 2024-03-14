62e1b8acaee704e299a630c877cf59e3f284ae3d
Jonathan Bezerra <jbbarroz@gmail.com>
2024-03-14 06:22:08 -0300

    Update config_setup_dev.sh

--- config_setup_dev.sh
+++ config_setup_dev.sh
@@ -59,6 +59,11 @@ sudo apt autoremove autoclean clean
 Sudo apt update -y
 sudo apt install git build-essential cmake git pkg-config libtool g++ libunibilium4 libunibilium-dev \
 ninja-build gettext libtool libtool-bin autoconf automake unzip curl doxygen lua-term lua-term-dev luarocks -y
+#Clone o repositorio e make (compile) o instalador
+git clone https://github.com/neovim/neovim
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
 
 
 
