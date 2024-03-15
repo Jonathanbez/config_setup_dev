30c376eddcf9ba6688348bc0c7a6c2e1f171ec21
Jonathan Bezerra <jbbarroz@gmail.com>
2024-03-15 06:49:32 -0300

    Update config_setup_dev.sh

--- config_setup_dev.sh
+++ config_setup_dev.sh
@@ -19,8 +19,6 @@ sudo apt install zsh -y
 # Instale o ohmyzsh (apos instalar o ohmyzsh ele ira pedir confirmação para tornar zsh como SHELL padrão)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
-
 # Instalar Zsh Autosuggestions
 # https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -31,6 +29,24 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # Incluir no campo plugins do .zshrc
 # Ex.: plugins=(zsh-autosuggestions zsh-syntax-highlighting)
+
+# Instale o gerenciador de versões Ruby, verifique o link do RMV para instalar o RVM
+# https://github.com/rvm/ubuntu_rvm
+sudo apt-get install software-properties-common
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo apt-get update -y
+sudo apt-get install rvm -y
+sudo usermod -a -G rvm $USER
+echo 'source "/etc/profile.d/rvm.sh"' >> ~/.zshrc
+
+# Aconselho a Reinicie o computador
+#reboot
+
+# Instale o Ruby
+rvm install 3.2.2
+
+# Instale o Rails
+gem install rails
 
 # Instalar NEOVIM (versao mais recente usando o repositorio oficial no GitHub)
 # Antes de tudo remova o Neovim do seu sistema (talvez ja tenha instalado por engano ou já venha com o OS usado)
